f84016d84cf5535a316ab797122da5c8b92b3626
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-19 20:52:39 +0100

    fixed bugs

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # This manifest executes the pkill command to terminate the 'killmenow' process.
 
 exec { 'killmenow':
- command    => '/usr/bin/pkill -f killmenow',
- onlyif => '/usr/bin/pgrep killmenow',
+  command    => '/usr/bin/pkill -f killmenow',
+  onlyif => '/usr/bin/pgrep killmenow',
 }
