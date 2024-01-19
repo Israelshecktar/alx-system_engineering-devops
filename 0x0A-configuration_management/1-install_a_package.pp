8f97acdc31e1b77dcc7474adbbba72383bcde492
Israelshecktar <iamshecktar1996@gmail.com>
2024-01-19 21:13:06 +0100

    fixed leaks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# This manifest installs Flask 2.0.2 and Werkzeug 2.2.2 using pip3.
-
-package { 'Flask':
-  ensure  => '2.0.2', # Specify the version of the package.
-  provider => 'pip3', # Specify the provider to use pip3 for Python 3.
- require => Package['python3-pip'],
-}
-
-package { 'Werkzeug':
-  ensure  => '2.2.2', # Specify the version of the package.
-  provider => 'pip3', # Specify the provider to use pip3 for Python 3.
- require => Package['python3-pip'],
+# A Puppet Script to install flask from pip3 and ensure its version 2.1.0
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
