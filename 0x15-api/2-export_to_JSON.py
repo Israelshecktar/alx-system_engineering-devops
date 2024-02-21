56b1a91719c9f0918921ab1ad377d987f846d8f6
Israelshecktar <iamshecktar1996@gmail.com>
2024-02-21 13:32:42 +0100

    api implementation

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,23 +12,23 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read().decode())
-        employeeName = data.get('name')
+        employeeName = data.get('username')
 
     todoUrl = url + "/todos"
 
     with urllib.request.urlopen(todoUrl) as response:
         data = json.loads(response.read().decode())
         tasks = data
-        done = 0
-        done_tasks = []
 
-        for task in tasks:
-            if task.get('completed'):
-                done_tasks.append(task)
-                done += 1
-
-        print("Employee {} is done with tasks({}/{}):"
-              .format(employeeName, done, len(tasks)))
-
-        for task in done_tasks:
-            print("\t {}".format(task.get('title')))
+    with open('{}.json'.format(employeeId), mode='w') as file:
+        json.dump(
+            {
+                employeeId: [
+                    {
+                        "task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": employeeName
+                    } for task in tasks
+                ]
+            },
+            file)
