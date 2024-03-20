fc67591530b946258991481ccfb9073dea44a876
Israelshecktar <Iamshecktar1996@gmail.com>
2024-03-20 10:59:25 +0100

    fixed puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,20 +1,19 @@
 # Define a class to manage Nginx configuration
 class nginx_config {
-
   # Set the desired ULIMIT value
   $ulimit_value = 4096
 
   # Define the file resource to manage /etc/default/nginx
   file { '/etc/default/nginx':
-    ensure  => file,
-    content => "ULIMIT=\"-n ${ulimit_value}\"\n", # Use the inline template
-    notify  => Service['nginx'], # Trigger Nginx restart when the file changes
+    ensure  => 'file',
+    content => "ULIMIT=\"-n ${ulimit_value}\"\n",
+    notify  => Service['nginx'],
   }
 
   # Define the service resource to manage Nginx service
   service { 'nginx':
-    ensure  => running,
-    enable  => true,
+    ensure => 'running',
+    enable => true,
   }
 }
 
